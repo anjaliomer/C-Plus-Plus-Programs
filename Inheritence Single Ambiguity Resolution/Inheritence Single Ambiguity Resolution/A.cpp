b76df091f5ff9a38f009e47c838deb85b3a92a84
anjaliomer <anjaliom@buffalo.edu>
2018-05-09 13:36:59 -0400

    Added Inheritence Single Ambiguity Resolution

---
+++ Inheritence Single Ambiguity Resolution/Inheritence Single Ambiguity Resolution/A.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "A.h"
+#include<iostream>
+
+using namespace std;
+
+void A::display()
+{
+	cout << "A" << endl;
+}
---
+++ Inheritence Single Ambiguity Resolution/Inheritence Single Ambiguity Resolution/B.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "B.h"
+#include<iostream>
+
+using namespace std;
+
+
+void B::display()
+{
+	cout << "B" << endl;
+}
